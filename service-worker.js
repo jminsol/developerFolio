58e1cad84052182e2ed58390fc5777d01cd96aea
jminsol <jminsol0525@gmail.com>
2021-09-11 08:02:14 +0000

    Deploying to gh-pages from  @ cded85c17faf822512d0704fd5b692431c25fa8a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//precache-manifest.fcc14060dd667bde338802b8cb8c1ac4.js"
+  "/precache-manifest.52ff2398f45408d220afe912862f106e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
